208f14c0e4cf2a782e9d6f24af31bcf1646e2af7
Fan <512942286@qq.com>
2020-01-06 16:54:21 +0800

    update run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,15 @@
-nohup python main.py --num_steps 1000000 --env-name Walker2d-v2 --seed 123456 --cuda > log_run1.txt 2>&1 &
+$ENVNAME=Hopper-v2
+
+for ((i=1; i<=5; i ++))
+do
+nohup python main.py --num_steps 1000000 --env-name $ENVNAME --seed $i --cuda --gpu 3 --alpha_f 0.1  > log_run_$i.txt 2>&1 &
 sleep 2
-nohup python main.py --num_steps 1000000 --env-name Walker2d-v2 --seed 234567 --cuda > log_run2.txt 2>&1 &
+done
+
+
+for ((i=1; i<=5; i ++))
+do
+nohup python main.py --num_steps 1000000 --env-name $ENVNAME --seed $i --cuda --gpu 5   > log_run_base_$i.txt 2>&1 &
 sleep 2
-nohup python main.py --num_steps 1000000 --env-name Walker2d-v2 --seed 345678 --cuda > log_run3.txt 2>&1 &
-sleep 2
-nohup python main.py --num_steps 1000000 --env-name Walker2d-v2 --seed 456789 --cuda > log_run4.txt 2>&1 &
-sleep 2
-nohup python main.py --num_steps 1000000 --env-name Walker2d-v2 --seed 567890 --cuda > log_run5.txt 2>&1 &
-sleep 2
+done
 
